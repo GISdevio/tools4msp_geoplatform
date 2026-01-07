0ff98cf118ec7386a591d7d1a888b96f655a2e2f
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2026-01-07 15:39:33 +0000

    start adding map import related functionality

--- legacy/importer.py
+++ legacy/importer.py
@@ -1,3 +1,7 @@
+import httpx
+from typing import Iterator
+
+
 def import_map():
     context = {
         "title": None,
@@ -10,3 +14,19 @@ def import_map():
         "groups": [],
         "maplayers": []
     }
+
+
+def gather_datasets_details_via_api(
+        http_client: httpx.Client,
+        page_size: int = 10
+) -> Iterator[dict]:
+    """Gather id and title of existing datasets"""
+    base_url = "https://dev.geoplatform.tools4msp.eu"
+    next_url  = f"{base_url}/api/v2/datasets/?page=1&page_size={page_size}"
+    while next_url:
+        dataset_list_response = http_client.get(next_url)
+        dataset_list_response.raise_for_status()
+        response_repr = dataset_list_response.json()
+        for dataset in dataset_list_response["datasets"]:
+            yield dataset
+        next_url = response_repr["links"]["next"]
